89e1e40c776c63717d6e0543304ba70ce6e91ea9
kkklesia <88797682+kkklesia@users.noreply.github.com>
2025-11-07 02:03:48 +0200

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,89 @@
+with sales as (
+select 
+	c.category_name, 
+	sum(od.unit_price * od.quantity * (1 - od.discount)) as total_sales
+from order_details od
+join products p on od.product_id = p.product_id
+join categories c on p.category_id = c.category_id
+group by c.category_name
+order by c.category_name
+),
+
+avg_order_value as (
+select
+	orders_per_category.category_name,
+	avg(orders_per_category.order_value) as avg_order_value
+from (
+		select 
+    		o.order_id,
+        	c.category_name,
+        	sum(od.unit_price * od.quantity * (1 - od.discount)) as order_value
+    	from orders o
+    	join order_details od on o.order_id = od.order_id
+    	join products p on od.product_id = p.product_id
+    	join categories c on p.category_id = c.category_id
+    	group by o.order_id, c.category_name
+	) as orders_per_category
+group by orders_per_category.category_name
+),
+
+avg_time_to_ship as (
+select
+	c.category_name,
+    avg(o.shipped_date - o.order_date) as avg_days_to_ship
+from orders o
+join order_details od on o.order_id = od.order_id
+join products p on od.product_id = p.product_id
+join categories c on p.category_id = c.category_id
+group by c.category_name
+),
+
+ranked_products as (
+select 
+	category_name,
+    product_id,
+    total_sales,
+    row_number() over (partition by category_name order by total_sales desc) as rn
+from (
+	select 
+		c.category_name,
+        p.product_id,
+        sum(od.unit_price * od.quantity * (1 - od.discount)) as total_sales
+    from order_details od
+    join products p on od.product_id = p.product_id
+    join categories c on p.category_id = c.category_id
+    group by c.category_name, p.product_id) 
+),
+
+top_5_ratio as (
+select
+	category_name,
+    sum(total_sales) filter (where rn <= 5) / sum(total_sales) as top_5_ratio
+from ranked_products
+group by category_name
+),
+
+avg_order_freq as (
+select 
+	c.category_name,
+    count(distinct o.order_id) / count(distinct o.customer_id) as avg_order_frequency
+from orders o
+join order_details od on o.order_id = od.order_id
+join products p on od.product_id = p.product_id
+join categories c on p.category_id = c.category_id
+group by c.category_name
+)
+
+select 
+    s.category_name,
+    round(s.total_sales::numeric, 0) as total_sales,
+    round(aov.avg_order_value::numeric, 0) as avg_order_value,
+    round(ats.avg_days_to_ship, 0) as avg_days_to_ship,
+    round(t.top_5_ratio::numeric * 100, 2) as top_5_ratio_percent,
+    aof.avg_order_frequency
+from sales s
+left join avg_order_value aov on s.category_name = aov.category_name
+left join avg_time_to_ship ats on s.category_name = ats.category_name
+left join top_5_ratio t on s.category_name = t.category_name
+left join avg_order_freq aof on s.category_name = aof.category_name
+order by s.total_sales desc;
